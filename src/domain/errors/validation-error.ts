b3826d0d59698ca55b54c01285c8a958b50356e3
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-03-19 00:49:12 -0300

    feat: add entity validator error

---
+++ src/domain/errors/validation-error.ts
@@ -0,0 +1,10 @@
+import { FieldsErrors } from '../validators';
+
+export class ValidationError extends Error {}
+
+export class EntityValidationError extends Error {
+  constructor(public error: FieldsErrors) {
+    super('Entity Validation Error');
+    this.name = 'EntityValidationError';
+  }
+}
